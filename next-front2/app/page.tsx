9d9ab97661e4dca7cb306660e24f3c92a29fbf03
diollia <diollia88@gmail.com>
2024-11-02 21:22:21 +0300

    refactoring of grid constructor

--- next-front2/app/page.tsx
+++ next-front2/app/page.tsx
@@ -2,57 +2,54 @@ import Image from "next/image";
 import {Metadata} from "next";
 import Link from "next/link";
 import {links} from "@/app/ui/main/navigation-links";
+import {Property} from "csstype";
+import Grid = Property.Grid;
 
 export const metadata: Metadata = {
     title: 'DIOLLIA',
 };
 
+class GridElement {
+    name: string;
+    href: string;
+    imageSrc: string
+
+    constructor(name: string, href: string, imageSrc: string) {
+        this.name = name;
+        this.href = href;
+        this.imageSrc = imageSrc
+    }
+}
+
+const movieGE = new GridElement("Movie", "/movie", "/lebowski.jpg");
+const gamesGE = new GridElement("Games", "/games", "/gothic.jpg");
+const musicGE = new GridElement("Music", "/music", "/rammstein.jpg");
+
+const sections = [movieGE,musicGE, gamesGE]
+
+function buildGridElement(section: GridElement){
+return (
+    <div key={section.name} className="flex flex-col gap-y-80">
+        <Link className="w-full h-full" href={section.href} passHref>
+            <div className="w-full h-full relative neon-border">
+                <Image
+                    src={section.imageSrc}
+                    alt={`${section.name} placeholder`}
+                    fill
+                    className="object-cover"
+                />
+                <p>{section.name}</p>
+            </div>
+        </Link>
+    </div>
+)
+}
+
 export default function Home() {
     return (
         <main>
             <div className="grid grid-cols-3 gap-0 h-screen">
-                <div className="flex flex-col gap-y-80 ">
-                    <Link className="w-full h-full"
-                          key={"Movies"}
-                          href={"/movies"}
-                          passHref>
-                        <div className="w-full h-full relative neon-border">
-                            <Image src="/Lebowski.jpg"
-                                   alt="lebowski placeholder"
-                                   layout="fill"
-                                   objectFit="cover"/>
-                            <p>Movies</p>
-                        </div>
-                    </Link>
-                </div>
-                <div className="flex flex-col gap-y-80">
-                    <Link className="w-full h-full"
-                          key={"Music"}
-                          href={"/music"}
-                          passHref>
-                        <div className="w-full h-full relative neon-border">
-                            <p>Music</p>
-                            <Image src="/rammstein.jpg"
-                                   alt="rammstein placeholder"
-                                   layout="fill"
-                                   objectFit="cover"/>
-                        </div>
-                    </Link>
-                </div>
-                <div className="flex flex-col gap-y-80">
-                    <Link className="w-full h-full"
-                          key={"Movies"}
-                          href={"/movies"}
-                          passHref>
-                        <div className="w-full h-full relative neon-border">
-                            <p>Games</p>
-                            <Image src="/gothic.jpg"
-                                   alt="gothic placeholder"
-                                   layout="fill"
-                                   objectFit="cover"/>
-                        </div>
-                    </Link>
-                </div>
+                {sections.map(section => buildGridElement(section))}
             </div>
             <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
                 <a
