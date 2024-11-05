00d7d87f2d2034f4e0298459fd8b7919748ea1da
diollia <diollia88@gmail.com>
2024-11-05 18:38:19 +0300

    change model memCard add route for image upload

---
+++ next-front/app/mems/upload/route.ts
@@ -0,0 +1,33 @@
+import {NextResponse} from 'next/server'
+import {MemModel} from "@/lib/definitions";
+
+
+export async function POST(req: Request): Promise<NextResponse> {
+    const formData = await req.formData()
+    const cardName = formData.get("name") as string
+    const cardImage = formData.get("image") as File
+    const cardDescription = formData.get("description") as string
+
+    if (!cardName || !cardImage || !cardDescription) {
+        return NextResponse.json({error: "All fields are required."}, {status: 400});
+    }
+
+    const imageBuffer = await cardImage.arrayBuffer()
+    const buffer = Buffer.from(imageBuffer)
+
+    try {
+        const memCard = new MemModel({
+            name: cardName,
+            image: {
+                data: buffer,
+                contentType: cardImage.type,
+            },
+            description: cardDescription
+        });
+        await memCard.save
+        return NextResponse.json({message: "Image uploaded successfully"}, {status: 201})
+    } catch (error){
+        return NextResponse.json({error: "An error occured during Image upload. Reason: " + error}, {status: 400})
+
+    }
+}
--- next-front/lib/definitions.ts
+++ next-front/lib/definitions.ts
@@ -3,15 +3,21 @@ import mongoose, {Document, Schema} from 'mongoose'
 export interface MemCard extends Document {
     //id: number; //is it realy needed
     name: string;
-    image: string;
+    image: {
+        data: Buffer;
+        contentType: string;
+    };
     description: string;
 }
 
 const memSchema: Schema = new Schema({
     name: { type: String, required: true },
-    image: { type: String, required: true },
+    image: {
+        data: Buffer,
+        contentType: String,
+    },
     description: { type: String, required: true },
 });
 
-// eslint-disable-next-line no-var
-export var MemModel = mongoose.models.mems_ || mongoose.model<MemCard>("mems_", memSchema);
+
+export const MemModel = mongoose.models.mems_ || mongoose.model<MemCard>("mems_", memSchema);
