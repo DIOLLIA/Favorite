2dc079d7c91b18b2e8eed5b0a94752229b6618c3
diollia <diollia88@gmail.com>
2024-11-15 17:11:10 +0300

    add music and movies modules

---
+++ TwoSmokingbarrels/src/main/java/lock/stock/twosmokingbarrels/TwoSmokingbarrelsApplication.java
@@ -0,0 +1,13 @@
+package lock.stock.twosmokingbarrels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoSmokingbarrelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwoSmokingbarrelsApplication.class, args);
+    }
+
+}
