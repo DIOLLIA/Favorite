bb43f656f7e6cdb23bfbbe056612ccf7c473dbf0
diollia <diollia88@gmail.com>
2025-07-30 15:52:41 +0300

    update app_config to search .env file because if project runs from terminal or IDE that will lead to errors add 2 files to run all projects in one command/place update readme
    TODO Add java kotlin

--- Nameless/not/even/citizen/internal/config/app_config.go
+++ Nameless/not/even/citizen/internal/config/app_config.go
@@ -3,11 +3,32 @@ package config
 import (
 	"github.com/joho/godotenv"
 	"log"
+	"os"
+	"path/filepath"
+	"runtime"
 )
 
 func LoadAppEnvVars() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	_, currentFile, _, _ := runtime.Caller(0)
+	dir := filepath.Dir(currentFile)
+
+	// looking for .env in parent directories
+	for {
+		envPath := filepath.Join(dir, ".env")
+		if _, err := os.Stat(envPath); err == nil {
+			err := godotenv.Load(envPath)
+			if err != nil {
+				log.Fatalf("Error loading .env file from %s: %v\n", envPath, err)
+			}
+			return
+		}
+
+		parent := filepath.Dir(dir)
+		if parent == dir {
+			break
+		}
+		dir = parent
 	}
+
+	log.Fatalf("No .env file found in any parent directory")
 }
